b5fd1078a0a1fd8404ec43e8197e78e5606002df
Andrew <bg43179@gmail.com>
2020-03-20 16:03:12 -0700

    ac - move string helper methods to helper

---
+++ gem_viewer/helper.rb
@@ -0,0 +1,21 @@
+# Copy from: https://www.rubydoc.info/gems/seqtrimnext/String:decamelize
+def decamelize(string)
+  string.to_s.
+    gsub(/([A-Z\d]+)([A-Z][a-z])/, '\1_\2').
+    gsub(/([a-z]+)([A-Z\d])/, '\1_\2').
+    gsub(/([A-Z]{2,})(\d+)/i, '\1_\2').
+    gsub(/(\d+)([a-z])/i, '\1_\2').
+    gsub(/(.+?)\&(.+?)/, '\1_&_\2').
+    gsub(/\s/, '_').downcase
+end
+
+# Copy from: https://apidock.com/rails/v5.2.3/ActiveSupport/Inflector/demodulize
+def demodulize(path)
+  path = path.to_s
+  if i = path.rindex("::")
+    path[(i + 2)..-1]
+  else
+    path
+  end
+end
+
